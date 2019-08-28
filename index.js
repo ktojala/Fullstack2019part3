1234a0fc52b777aa78ce3c4a15fc130d2c62941e
Kari Ojala <kari.ojala@helsinki.fi>
2019-08-28 20:13:18 +0300

    Exercise 3.3

--- index.js
+++ index.js
@@ -39,6 +39,17 @@ app.get('/info', (req, res) => {
   res.end()
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+
+})
+
 const port = 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
